8e917ad2c6c2829e245188ad49d436aa1ea2c85d
PedroHenriqueDuart3 <pedro.duarte@germinare.org.br>
2026-03-11 09:55:09 -0300

    chore bugfix: ajustando url API Render

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://web-escola.onrender.com',
+        target: 'https://web-escolar.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
